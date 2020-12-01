a5ee8c277e02d0eb531444d2f245ccb4c55cdf94
Daniel-Penaloza password = d4n13lpm <=>
2020-12-01 12:27:49 -0800

    error 500

--- web/index.php
+++ web/index.php
@@ -33,37 +33,17 @@
     function stablishConection(){
 
         //Get Heroku ClearDB connection information
-        $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server   = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db       = substr($cleardb_url["path"],1);
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
 
-        $active_group = 'default';
-        $query_builder = TRUE;
+        $conn = new mysqli($server, $username, $password, $db);
 
-        $db['default'] = array(
-            'dsn'    => '',
-            'hostname' => $cleardb_server,
-            'username' => $cleardb_username,
-            'password' => $cleardb_password,
-            'database' => $cleardb_db,
-            'dbdriver' => 'mysqli',
-            'dbprefix' => '',
-            'pconnect' => FALSE,
-            'db_debug' => (ENVIRONMENT !== 'production'),
-            'cache_on' => FALSE,
-            'cachedir' => '',
-            'char_set' => 'utf8',
-            'dbcollat' => 'utf8_general_ci',
-            'swap_pre' => '',
-            'encrypt' => FALSE,
-            'compress' => FALSE,
-            'stricton' => FALSE,
-            'failover' => array(),
-            'save_queries' => TRUE
-        );
+        return $conn;
+
         
         /*
         // Creando variables para nuestra conexion a MySQL
