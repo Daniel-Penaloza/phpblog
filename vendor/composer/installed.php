9208961e97ce7a0d0daadb498b573d4602b07e4d
Daniel-Penaloza password = d4n13lpm <=>
2020-12-01 12:10:36 -0800

    500

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bddb7728856cb951d3d5f44f1963386508150214',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bddb7728856cb951d3d5f44f1963386508150214',
+    ),
+  ),
+);
